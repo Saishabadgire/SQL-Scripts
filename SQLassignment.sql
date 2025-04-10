6910ce2e7633320dc7d0db4d5c08ca67a34e7bd5
Saisha Badgire <saishabadgire06@gmail.com>
2025-04-10 13:49:00 +0530

    Add files via upload

---
+++ SQLassignment.sql
@@ -0,0 +1,179 @@
+--assignment 2
+--1
+CREATE TABLE Customer1 (
+    customer_id INT,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    address VARCHAR(255),
+    city VARCHAR(100),
+    state VARCHAR(100),
+    zip VARCHAR(20)
+);   --2
+INSERT INTO Customer1 (customer_id,first_name, last_name, email, address, city, state, zip) VALUES 
+(1,'John', 'Doe', 'john.doe@gmail.com', 'unique colony', 'New York', 'NY', '10001'),
+(2,'Jane', 'Smith', 'jane.smith@gnail..com', 'sunshine colony', 'Los Angeles', 'CA', '90001'),
+(3,'Alice', 'Brown', 'alice.brown@gmail.com', 'gelexy building', 'san jose', 'california', '60601'),
+(4,'jeni', 'Sword', 'jeni.s@gnail..com', 'moon colony', 'san jose', 'Cl', '34657'),
+(5,'rutu','handargule','rutu22@gmail.com','gandhi chowk','beed','maharashtra','43556');
+
+select*from customer1
+--3
+select first_name,last_name 
+from customer
+--4
+
+select*
+from customer1 
+where first_name like 'g%' and city ='san jose';
+
+SELECT *
+FROM Customer1
+WHERE email LIKE '%@gmail.com';
+
+SELECT * 
+FROM Customer1
+WHERE last_name NOT LIKE '%A';
+
+--assignment3
+--1
+CREATE TABLE Orders (
+    order_id INT,
+    order_date DATE NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
+    customer_id INT,);
+	--2
+	INSERT INTO Orders (order_id,order_date, amount, customer_id) VALUES 
+(101,'2024-02-20', 250.50, 1),
+(102,'2024-02-21', 100.75, 2),
+(103,'2024-02-22', 300.00, 1),
+(104,'2024-02-23', 450.25, 3),
+(105,'2024-02-24', 500.00, 2);
+
+select*from orders
+
+--3
+SELECT Customer1.customer_id, Customer1.first_name, Customer1.last_name, Customer1.email, Orders.order_id, Orders.order_date, Orders.amount
+FROM Customer1
+INNER JOIN Orders ON Customer1.customer_id = Orders.customer_id;
+
+--4
+--right join
+
+SELECT Customer1.customer_id, Customer1.first_name, Customer1.last_name, Customer1.email, Orders.order_id, Orders.order_date, Orders.amount
+FROM Customer1
+RIGHT JOIN Orders ON Customer1.customer_id = Orders.customer_id;
+
+--left join
+
+SELECT Customer1.customer_id, Customer1.first_name, Customer1.last_name, Customer1.email, Orders.order_id, Orders.order_date, Orders.amount
+FROM Customer1
+left JOIN Orders ON Customer1.customer_id = Orders.customer_id;
+
+--outer join
+--5
+SELECT Customer1.customer_id, Customer1.first_name, Customer1.last_name, Customer1.email, Orders.order_id, Orders.order_date, Orders.amount
+FROM Customer1
+left JOIN Orders ON Customer1.customer_id = Orders.customer_id
+union
+SELECT Customer1.customer_id, Customer1.first_name, Customer1.last_name, Customer1.email, Orders.order_id, Orders.order_date, Orders.amount
+FROM Customer1
+RIGHT JOIN Orders ON Customer1.customer_id = Orders.customer_id;
+
+--6
+UPDATE Orders SET amount = 100 WHERE customer_id = 3;
+select*from orders
+
+
+--assignment 4
+
+create table orders_table
+(
+order_id int
+,customer_name varchar(100)
+);
+alter table orders_table
+add amount int
+insert into orders_table(order_id,customer_name,amount)
+values (101,'alice',500),
+      (102,'bob',1200),
+	  (103,'charlie',800),
+	  (104,'devid',300),
+	  (105,'emma',1500);
+
+	  select*from orders_table
+
+	  create table employee_details1
+	  (
+	  emp_id int
+	  ,emp_name varchar(100)
+	  ,emp_salary int
+	  );
+
+	  insert into employee_details1(emp_id,emp_name,emp_salary)
+	  values(1,'john',50000),
+	        (2,'sarah',60000),
+			(3,'mike',70000),
+			(4,'emma',80000),
+			(5,'robert',55000);
+
+			select *from employee_details1
+
+			create table employee_details2
+			(
+			emp_id int
+			,emp_name varchar(100)
+			,emp_salary int
+			);
+
+			insert into employee_details2(emp_id,emp_name,emp_salary)
+			values (3,'mike',70000),
+			      (4,'emma',80000),
+				  (5,'robert',55000),
+				  (6,'jessika',65000),
+				  (7,'tom',75000);
+
+				  select*from employee_details2
+
+				  --1
+				  SELECT 
+    MIN(amount) AS min_amount,
+    MAX(amount) AS max_amount,
+    AVG(amount) AS avg_amount
+FROM orders;
+
+--2
+CREATE FUNCTION MultiplyByTen (@num INT)  
+RETURNS INT  
+AS  
+BEGIN  
+    RETURN @num * 10;  
+END;
+
+--3
+SELECT 
+    CASE 
+        WHEN 100 < 200 THEN '100 is less than 200'
+        WHEN 100 > 200 THEN '100 is greater than 200'
+        WHEN 100 = 200 THEN '100 is equal to 200'
+        ELSE 'Unknown'
+    END AS result;
+
+	--4
+	SELECT order_id, customer_name, amount,
+    CASE 
+        WHEN amount >= 1000 THEN 'High Amount'
+        WHEN amount BETWEEN 500 AND 999 THEN 'Medium Amount'
+        ELSE 'Low Amount'
+    END AS amount_status
+FROM orders_table;
+
+--5
+
+CREATE FUNCTION GetAmountsGreaterThan (@inputAmount DECIMAL(10,2))  
+RETURNS TABLE  
+AS  
+RETURN  
+(  
+    SELECT * FROM orders_table WHERE amount > @inputAmount  
+);
---
+++ realational database design.sql
@@ -0,0 +1,92 @@
+create table role
+(
+id int primary key
+,role_name varchar(100)
+);
+
+create table user_account
+(
+id int primary key
+,username varchar (100)
+,email varchar(254)
+,password varchar(200)
+,password_salt varchar(50) not null
+,password_hash_algorithm varchar(50)
+
+);
+
+
+create  table status1
+(
+id int primary key
+,status_name varchar(100)
+,is_user_workinng bit 
+)
+
+Create table User_has_role
+(Id int Primary Key,
+role_start_time datetime,
+role_end_time datetime not null,
+user_account_id int foreign key References user_account(Id),
+role_id int foreign key references role(id));
+select*from User_has_role
+
+
+create table User_has_status
+(Id int primary key,
+status_start_time datetime,
+status_end_time datetime not null,
+user_account_id int foreign key references user_account(id),
+status_id int foreign key references status(id));
+select*from User_has_status
+
+insert into role(id,role_name)
+values (5,'sale'),
+  ( 3,'technical')
+select*from role
+
+insert into status(id,status_name,is_user_working)
+values (1,'new status',1),
+       (2,'new status',1)
+	   select *from status
+
+insert into user_account(id,username,email,password,password_salt,password_hash_algorithm)
+values (101,'saisha','saisha@gmail.com','saisha01','sai','abc'),
+      (102,'dnyana','dnyanu@gmail.com','dnyana00','dna','abd')
+ select*from user_account
+
+INSERT into User_has_role(Id, role_start_time, role_end_time,user_account_id,role_id)
+VALUES
+(10, '2023-04-12 01:45:22', '2024-05-22 02:45:22', 101, 5),
+(11, '2021-11-10 03:45:22', '2026-05-22 02:45:22', 102, 3);
+
+SELECT * FROM user_has_role;
+
+insert into user_has_status
+values(110, '2023-04-12 01:45:22', '2024-05-22 02:45:22', 101, 1),
+(111, '2021-11-10 03:45:22', '2026-05-22 02:45:22', 102, 2);
+SELECT * FROM user_has_status;
+
+alter table user_has_role
+drop constraint FK__User_has___role___253C7D7E
+
+alter table user_has_role
+drop constraint FK__User_has___user___24485945
+
+alter table user_has_role
+drop constraint PK__User_has__3214EC07421E5368
+
+alter table user_has_status
+drop constraint FK__User_has___statu__290D0E62
+
+alter table user_has_status
+drop constraint FK__User_has___user___2818EA29
+
+alter table user_has_status
+drop constraint PK__User_has__3214EC07352129A4
+ 
+ alter table user_account
+ drop constraint PK__user_acc__3213E83F89ED8020
+
+drop table role
+drop table status
